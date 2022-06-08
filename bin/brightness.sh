1333c408afbac492533fa7a18076b68000f79e2a
Angel Segarra <angel.segarra@gmail.com>
2022-06-08 15:26:42 -0400

    update sway config

---
+++ bin/brightness.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+    echo "Need 1 argument (up, down)"
+    exit 1
+fi
+
+notify() {
+    local current_level="$(light -G)"
+    local icon='notification-display-brightness'
+    notify-send -i $icon -h string:x-dunst-stack-tag:light -h int:value:$current_level $(printf %.0f%% $current_level)
+}
+
+case "$1" in
+    up) 
+        light -A 5
+	notify 
+	;;
+    down) 
+	light -U 5
+	notify 
+	;;
+    *) echo "$1 is not a valid option" ;;
+esac
+
