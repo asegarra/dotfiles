5a28a5204421faf9ec300f3788cca97cbbfeb816
Angel Segarra <angel.segarra@gmail.com>
2022-07-15 11:41:04 -0400

    stuff

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -9,6 +9,7 @@ typeset -gU cdpath fpath mailpath path
 # Set the list of directories that Zsh searches for programs.
 path=(
   $HOME/{,s}bin(N)
+  $HOME/.local/bin
   /opt/local/{,s}bin(N)
   /usr/local/{,s}bin(N)
   $HOME/.cargo/bin
